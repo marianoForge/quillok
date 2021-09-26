adf99a4a90e6385d704909a0adcaf1b0044f2116
Mariano De Simone <marianods@MSI-MDS.localdomain>
2021-09-26 19:50:42 -0300

    AOS 2

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql, useStaticQuery } from "gatsby"
 // import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as servicesStyles from "../styles/services.module.css"
@@ -6,9 +6,12 @@ import AOS from "aos"
 import "aos/dist/aos.css"
 
 const Services = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    })
+  }, [])
 
-  AOS.init()
-  
   const data = useStaticQuery(graphql`
     query MyQuery {
       allMarkdownRemark(sort: { order: ASC, fields: frontmatter___id }) {
@@ -39,21 +42,32 @@ const Services = () => {
   return (
     <div className={servicesStyles.flex_container_fluid}>
       <div className={servicesStyles.flex_container}>
-
         <h2 className={servicesStyles.titleSection}>SERVICES</h2>
-        <p className={servicesStyles.subtitleSection}>Explore what we offer...</p>
-        <div className={servicesStyles.containerServices} >
-        {services.map(service => (
-          <div className={servicesStyles.pointServices} data-aos="fade-up" data-aos-delay="0" data-aos-duration="1000">
-            <div className={servicesStyles.numberTitle} data-aos="zoom-in" data-aos-delay="500" data-aos-duration="1000">0{service.frontmatter.id}</div>
-            <h2>{service.frontmatter.title}</h2>
-            <p>{service.internal.content}</p>
-          </div>
-        ))}
+        <p className={servicesStyles.subtitleSection}>
+          Explore what we offer...
+        </p>
+        <div className={servicesStyles.containerServices}>
+          {services.map(service => (
+            <div
+              className={servicesStyles.pointServices}
+              data-aos="fade-up"
+              data-aos-delay="0"
+              data-aos-duration="1000"
+            >
+              <div
+                className={servicesStyles.numberTitle}
+                data-aos="zoom-in"
+                data-aos-delay="500"
+                data-aos-duration="1000"
+              >
+                0{service.frontmatter.id}
+              </div>
+              <h2>{service.frontmatter.title}</h2>
+              <p>{service.internal.content}</p>
+            </div>
+          ))}
         </div>
-
       </div>
-
     </div>
   )
 }
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,94 +1,139 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { StaticImage } from "gatsby-plugin-image"
 import * as testimonialStyles from "../styles/testimonials.module.css"
 import AOS from "aos"
 import "aos/dist/aos.css"
 
 const Testimonials = () => {
-
-  AOS.init()
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    })
+  }, [])
 
   return (
     <div className={testimonialStyles.flex_container_fluid}>
       <div className={testimonialStyles.flex_container}>
-
-        <div className={testimonialStyles.strokeBox} data-aos="flip-left" data-aos-delay="0" data-aos-duration="1500">
+        <div
+          className={testimonialStyles.strokeBox}
+          data-aos="flip-left"
+          data-aos-delay="0"
+          data-aos-duration="1500"
+        >
           <div className={testimonialStyles.testimonialBox}>
-
             <div className={testimonialStyles.boxHeader}>
               <div className={testimonialStyles.testimonialPhoto}>
                 <StaticImage src="../images/testimonial_1_tom.png" />
               </div>
               <div className={testimonialStyles.testimonialPerson}>
-                <span className={testimonialStyles.testimonialName}>Tom Belote</span>
-                <p className={testimonialStyles.testimonialPosition}>ShelfSet Founder & CEO</p>
+                <span className={testimonialStyles.testimonialName}>
+                  Tom Belote
+                </span>
+                <p className={testimonialStyles.testimonialPosition}>
+                  ShelfSet Founder & CEO
+                </p>
               </div>
             </div>
 
             <div className={testimonialStyles.boxBody}>
-              <p>“Quillok has great engineers that craft elegant mobile applications.<br></br>
-              At ShelfSet we were able to leverage them to build a product from concept to product launch in just a few months.</p>
+              <p>
+                “Quillok has great engineers that craft elegant mobile
+                applications.<br></br>
+                At ShelfSet we were able to leverage them to build a product
+                from concept to product launch in just a few months.
+              </p>
 
-              <p>They became experts in our product area in the specialized retail industry with machine learning computer vision.</p>
+              <p>
+                They became experts in our product area in the specialized
+                retail industry with machine learning computer vision.
+              </p>
             </div>
-
           </div>
         </div>
 
-        <div className={testimonialStyles.strokeBox} data-aos="flip-left" data-aos-delay="300" data-aos-duration="1500">
+        <div
+          className={testimonialStyles.strokeBox}
+          data-aos="flip-left"
+          data-aos-delay="300"
+          data-aos-duration="1500"
+        >
           <div className={testimonialStyles.testimonialBox}>
-
             <div className={testimonialStyles.boxHeader}>
               <div className={testimonialStyles.testimonialPhoto}>
-                <StaticImage src="../images/testimonial_2_roy.png"/>
+                <StaticImage src="../images/testimonial_2_roy.png" />
               </div>
               <div className={testimonialStyles.testimonialPerson}>
-                <span className={testimonialStyles.testimonialName}>Roy Houston</span>
-                <p className={testimonialStyles.testimonialPosition}>BugLox Founder</p>
+                <span className={testimonialStyles.testimonialName}>
+                  Roy Houston
+                </span>
+                <p className={testimonialStyles.testimonialPosition}>
+                  BugLox Founder
+                </p>
               </div>
             </div>
 
             <div className={testimonialStyles.boxBody}>
-              <p>“Quillok has great engineers that craft elegant mobile applications.<br></br>
-              At ShelfSet we were able to leverage them to build a product from concept to product launch in just a few months.</p>
+              <p>
+                “Quillok has great engineers that craft elegant mobile
+                applications.<br></br>
+                At ShelfSet we were able to leverage them to build a product
+                from concept to product launch in just a few months.
+              </p>
 
-              <p>They became experts in our product area in the specialized retail industry with machine learning computer vision.</p>
+              <p>
+                They became experts in our product area in the specialized
+                retail industry with machine learning computer vision.
+              </p>
             </div>
-
           </div>
         </div>
 
         <div className={testimonialStyles.testimonialTitle}>
-              <h2 className={testimonialStyles.titleSection}>TESTIMONIALS</h2>
-              <p className={testimonialStyles.subtitleSection}>What our customers say</p>
-              <div className={testimonialStyles.imgBGtitle}>
-                <StaticImage src="../images/testimonial_bg_title.svg" />
-              </div>
+          <h2 className={testimonialStyles.titleSection}>TESTIMONIALS</h2>
+          <p className={testimonialStyles.subtitleSection}>
+            What our customers say
+          </p>
+          <div className={testimonialStyles.imgBGtitle}>
+            <StaticImage src="../images/testimonial_bg_title.svg" />
           </div>
+        </div>
 
-          <div className={testimonialStyles.strokeBox} data-aos="flip-left" data-aos-delay="600" data-aos-duration="1500">
+        <div
+          className={testimonialStyles.strokeBox}
+          data-aos="flip-left"
+          data-aos-delay="600"
+          data-aos-duration="1500"
+        >
           <div className={testimonialStyles.testimonialBox}>
-
             <div className={testimonialStyles.boxHeader}>
               <div className={testimonialStyles.testimonialPhoto}>
-                <StaticImage src="../images/testimonial_3_shakur.png"/>
+                <StaticImage src="../images/testimonial_3_shakur.png" />
               </div>
               <div className={testimonialStyles.testimonialPerson}>
-                <span className={testimonialStyles.testimonialName}>Shakur Abdal-Khallaq</span>
-                <p className={testimonialStyles.testimonialPosition}>FaceFwd Founder & CEO</p>
+                <span className={testimonialStyles.testimonialName}>
+                  Shakur Abdal-Khallaq
+                </span>
+                <p className={testimonialStyles.testimonialPosition}>
+                  FaceFwd Founder & CEO
+                </p>
               </div>
             </div>
 
             <div className={testimonialStyles.boxBody}>
-              <p>“Quillok has great engineers that craft elegant mobile applications.<br></br>
-              At ShelfSet we were able to leverage them to build a product from concept to product launch in just a few months.</p>
+              <p>
+                “Quillok has great engineers that craft elegant mobile
+                applications.<br></br>
+                At ShelfSet we were able to leverage them to build a product
+                from concept to product launch in just a few months.
+              </p>
 
-              <p>They became experts in our product area in the specialized retail industry with machine learning computer vision.</p>
+              <p>
+                They became experts in our product area in the specialized
+                retail industry with machine learning computer vision.
+              </p>
             </div>
-
           </div>
         </div>
-        
       </div>
     </div>
   )
